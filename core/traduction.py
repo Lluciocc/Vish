280bc7ca48f7c4323b0467825e468ba797415268
Claudiney Gustavo Rodrigues dos Santos <87202002+Claudiney-Santos@users.noreply.github.com>
2026-04-19 23:04:13 -0300

    Translation Settings Backend-Improvement (#94)

--- core/traduction.py
+++ core/traduction.py
@@ -1,12 +1,33 @@
 import json
 from core.debug import Info, Debug
+import os
 
 class Traduction:
+    langs_path = Info.resource_path('assets/models/lang')
     model = {}
-    
+
+    @staticmethod
+    def get_languages():
+        langs = []
+        for file in os.scandir(Traduction.langs_path):
+            if file.is_file() and file.name[-5:] == '.json' and file.name != 'en.json':
+                try:
+                    with open(file.path, encoding='utf-8') as data:
+                        lang = json.load(data)
+                        lang_name = lang.get('language_name', 'Unknown language')
+                        file_name = file.name[:-5]
+                        langs.append((lang_name, file_name))
+                except json.JSONDecodeError:
+                    Debug.Error(f'{file.path} is not a valid json file')
+                except UnicodeDecodeError:
+                    Debug.Error(f'{file.path} is not compatible with unicode encoding')
+        langs.sort(key=lambda pair : pair[1])
+        langs.insert(0, ('English', 'en'))
+        return langs
+
     @staticmethod
     def set_translate_model(lang: str):
-        trad_file = Info.resource_path(f"assets/models/{lang}.json")
+        trad_file = os.path.join(Traduction.langs_path, lang + '.json')
         try:
             with open(trad_file, encoding='utf-8') as data:
                 Traduction.model = json.load(data)
