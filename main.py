8b95b7b55d9726e924ffe19c32c83705fa34b59c
Lluciocc <llucio.cc00@gmail.com>
2026-09-13 19:38:50 +0200

    fix: import deleted by ruff
    Signed-off-by: Lluciocc <llucio.cc00@gmail.com>

--- main.py
+++ main.py
@@ -46,12 +46,15 @@ from core.logger import Logger
 from core.projects import ProjectManager
 from core.serializer import Serializer
 from core.traduction import Traduction
-from nodes.command_nodes import EchoNode
-from nodes.operation_nodes import Addition
+
+from nodes import (  # noqa: F401
+    command_nodes,
+    operation_nodes,
+    utils_node,
+    variable_nodes,
+)
 from nodes.flow_nodes import StartNode
 from nodes.registry import NODE_REGISTRY
-from nodes.utils_node import ToString
-from nodes.variable_nodes import FileExistsNode
 from themes.theme_manager import Theme
 from ui.about.about import AboutDialog
 from ui.comment_box import COMMENT_Z_BASE, CommentBoxItem
